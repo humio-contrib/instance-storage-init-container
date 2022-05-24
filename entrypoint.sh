bc54f36177cd4c5c27c64d885585c44b96a784c2
Ryan Faircloth <ryan@dss-i.com>
2022-05-24 11:12:11 -0400

    feat: allow safe re-run

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ echo ##BEGIN LIST OF NVME
 nvme list
 echo ##END LIST OF NVME
 
+vgscan | grep VGInstance
+status=$?
+[ $status -eq 0 ] && "VG exists"; exit 0
 declare -r disks=($(nvme list | grep Instance | cut -f 1 -d ' '))
 if (( ${#disks[@]} )); then
     for i in "${disks[@]}"
