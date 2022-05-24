6f088bdf0b7d8d4be7e1da5ec004e708933ab1c1
Ryan Faircloth <ryan@dss-i.com>
2022-05-24 11:56:01 -0400

    fix: exit check

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ echo ##END LIST OF NVME
 
 vgscan | grep VGInstance
 status=$?
-[ $status -eq 0 ] && echo "VG exists" || echo "VG does not exist"
-[ $status -eq 0 ] exit 0
+[ $status -eq 1 ] && echo "VG exists" || echo "VG does not exist"
+[ $status -eq 1 ] exit 0
 declare -r disks=($(nvme list | grep Instance | cut -f 1 -d ' '))
 if (( ${#disks[@]} )); then
     for i in "${disks[@]}"
