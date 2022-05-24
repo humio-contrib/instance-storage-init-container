18c0f11e9ad33ea5fd7ad17e6089390ca615faaa
Ryan Faircloth <ryan@dss-i.com>
2022-05-24 12:13:50 -0400

    fix: fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-set -eu
-echo ##BEGIN LIST OF NVME
+echo BEGIN LIST OF NVME
 nvme list
-echo ##END LIST OF NVME
+echo END LIST OF NVME
 
-vgscan | grep VGInstance
-status=$?
-[ $status -eq 1 ] && echo "VG exists" || echo "VG does not exist"
-[ $status -eq 1 ] exit 0
-declare -r disks=($(nvme list | grep Instance | cut -f 1 -d ' '))
-if (( ${#disks[@]} )); then
-    for i in "${disks[@]}"
-    do
-        echo "Creating PV $i"
-        pvcreate -ff -y $i
-    done
+OUTPUT=$(vgscan 2> /dev/null | grep VGInstance)
+if [ -z "$OUTPUT" ]
+then
+    echo "VG does not exist"
+    declare -r disks=($(nvme list | grep Instance | cut -f 1 -d ' '))
+    if (( ${#disks[@]} )); then
+        for i in "${disks[@]}"
+        do
+            echo "Creating PV $i"
+            pvcreate -ff -y $i
+        done
 
 
-    echo "Creating VG=VGInstance $disks"
-    vgcreate VGInstance $disks
+        echo "Creating VG=VGInstance $disks"
+        vgcreate VGInstance $disks
+    fi
+else
+    echo "VG exists"
 fi
+
+sleep infinity
